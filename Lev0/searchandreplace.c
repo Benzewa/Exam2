bbb7e0463990c491878af7a24abc2d85eaeb096a
Benzewa <mzsamy22@gmail.com>
2025-03-26 00:42:57 +0400

    Up

---
+++ Lev0/searchandreplace.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void SchRep(char *Str, char LetterOne, char LetterTwo);
+
+int main()
+{
+}
+void SchRep(char *Str, char LetterOne, char LetterTwo)
+{
+    char *ptr1 = str;
+    char *ptr2 = str;
+    while (*Str)
+    {
+
+        ptr++;
+    }
+}
