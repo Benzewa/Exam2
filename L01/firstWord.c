6859d351640d198e2fef760b760da208460516c8
Benzewa <mzsamy22@gmail.com>
2025-03-28 16:35:22 +0400

    Up

---
+++ L01/firstWord.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+void first_word(char *s)
+{
+    while (*s == ' ' || *s == '\t')
+        s++;
+    while (*s != ' ' && *s != '\t')
+    {
+        write(1, s, 1);
+        s++;
+    }
+}
+int main(int ac, char **av)
+{
+    if (ac == 2)
+    {
+        first_word(av[1]);
+    }
+    write(1, "\n", 1);
+}
